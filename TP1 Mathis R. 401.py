c7a0a0e8686724361e272b6360f48be97667e180
robinetm <112488077+robinetm@users.noreply.github.com>
2022-09-14 09:57:21 -0400

    Update TP1 Mathis R. 401.py

--- TP1 Mathis R. 401.py
+++ TP1 Mathis R. 401.py
@@ -1,10 +1,15 @@
-chaine = input("ecrit un mot ou des mots:")
+# Crée par Mathis Robinet
+# Crée en 22/09/14
+# TP1
+
+chaine = input("ecris un mot ou des mots en verifiant qu'il n'y ai d'espace qu'entre les mots:")
 
 
-def espace():
-    print ( " " in chaine )
-
-
-
-if espace() == True :
-    print("oui")
+def word_count():
+    espace = (" " in chaine)
+    if espace == True:
+        NbrEspace = (chaine.count(" "))
+        NbrMots = NbrEspace + 1
+        print("il y a %d mots dans ce que vos avez écris." % (NbrMots))
+        
+word_count()
